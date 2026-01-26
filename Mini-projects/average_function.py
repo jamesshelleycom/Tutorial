68a9a16eeaa7c36941ddeb19dd56661410556998
James Shelley <james.shelley@gmail.com>
2026-01-26 16:42:02 -0500

    Create average_function.py

---
+++ Mini-projects/average_function.py
@@ -0,0 +1,7 @@
+def average_function(numbers) :
+    average = sum(numbers) / len(numbers)
+    return average
+
+number_list = (321,324,22,54,22)
+
+print("The average is", average_function(number_list))
